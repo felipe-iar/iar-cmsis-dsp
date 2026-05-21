19f09c445586a9b16995c9d10b3bc66879015e0a
Felipe Torrezan <felipe.torrezan@qt.io>
2026-05-21 13:27:12 +0200

    examples: Update CMake scripts (#9)

--- Examples/cmake-all.sh
+++ Examples/cmake-all.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 
-CFG=(cortexM0l cortexM0b cortexM3l cortexM3b cortexM4l cortexM4b cortexM4lf cortexM4bf cortexM7l cortexM7b cortexM7ls cortexM7bs cortexM7lf cortexM7bf ARMv8MBLl ARMv8MMLl ARMv8MMLldfsp ARMv8MMLldfdp ARMv81MMLld ARMv81MMLldfdp)
-export IAR_CMSIS_DSP=/mnt/c/ProgramData/IARSystems/github.com/IAR-CMSIS-DSP
+CFG=(
+  cortexM0l
+  cortexM0b
+  cortexM3l
+  cortexM3b
+  cortexM4l
+  cortexM4b
+  cortexM4lf
+  cortexM4bf
+  cortexM7l
+  cortexM7b
+  cortexM7ls
+  cortexM7bs
+  cortexM7lf
+  cortexM7bf
+  ARMv8MBLl
+  ARMv8MMLl
+  ARMv8MMLldfsp
+  ARMv8MMLldfdp
+  ARMv81MMLld
+  ARMv81MMLldfdp)
 
-cmake -Bbuild -G"Ninja Multi-Config" --toolchain iccarm.cmake
+export CMAKE_GENERATOR="Ninja Multi-Config"
+cmake -Bbuild
 
-for a in ${CFG[@]}; do
-  cmake --build build --config $a $1
+for cfg in ${CFG[@]}; do
+  cmake --build build --config ${cfg} --clean-first $1
 done
 
--- Examples/cspybat.cmake
+++ Examples/cspybat.cmake
@@ -1,28 +1,35 @@
-# Execute `cspybat` via `ctest`
+# CTest wrapper for CSpyBat
+
+cmake_minimum_required(VERSION 3.20)
 
 # Enable CTest
 enable_testing()
 
+cmake_path(GET CMAKE_C_COMPILER PARENT_PATH BIN_DIR)
+cmake_path(GET BIN_DIR PARENT_PATH TOOLKIT_DIR)
+cmake_path(GET TOOLKIT_DIR FILENAME TOOLKIT)
+cmake_path(GET TOOLKIT_DIR PARENT_PATH IAR_ROOT_DIR)
+set(COMMON_BIN_DIR "${IAR_ROOT_DIR}/common/bin")
+
 function(iar_cspy_test TARGET RESULT)
   find_program(CSPY_BAT
-    NAMES cspybat CSpyBat
-    PATHS ${TOOLKIT_DIR}/../common
-    PATH_SUFFIXES bin
+    NAMES CSpyBat${CMAKE_HOST_EXECUTABLE_SUFFIX}
+    PATHS ${COMMON_BIN_DIR}
     REQUIRED)
-  find_program(CSPY_DRV_PROC
-    NAMES    ${CMAKE_SYSTEM_PROCESSOR}proc.dll    ${CMAKE_SYSTEM_PROCESSOR}PROC.dll
-          lib${CMAKE_SYSTEM_PROCESSOR}proc.so  lib${CMAKE_SYSTEM_PROCESSOR}PROC.so
-    PATHS ${TOOLKIT_DIR}/bin
+  find_library(CSPY_DRV_PROC
+    NAMES ${CMAKE_SHARED_LIBRARY_PREFIX}${TOOLKIT}PROC${CMAKE_SHARED_LIBRARY_SUFFIX}
+          ${CMAKE_SHARED_LIBRARY_PREFIX}${TOOLKIT}proc${CMAKE_SHARED_LIBRARY_SUFFIX}
+    PATHS ${BIN_DIR}
     REQUIRED)
-  find_program(CSPY_DRV_SIM
-    NAMES    ${CMAKE_SYSTEM_PROCESSOR}sim2.dll    ${CMAKE_SYSTEM_PROCESSOR}SIM2.dll
-          lib${CMAKE_SYSTEM_PROCESSOR}sim2.so  lib${CMAKE_SYSTEM_PROCESSOR}SIM2.so
-    PATHS ${TOOLKIT_DIR}/bin
+  find_library(CSPY_DRV_SIM
+    NAMES ${CMAKE_SHARED_LIBRARY_PREFIX}${TOOLKIT}SIM2${CMAKE_SHARED_LIBRARY_SUFFIX}
+          ${CMAKE_SHARED_LIBRARY_PREFIX}${TOOLKIT}sim2${CMAKE_SHARED_LIBRARY_SUFFIX}
+    PATHS ${BIN_DIR}
     REQUIRED)
-  find_program(CSPY_DRV_BAT
-    NAMES    ${CMAKE_SYSTEM_PROCESSOR}bat.dll    ${CMAKE_SYSTEM_PROCESSOR}Bat.dll
-          lib${CMAKE_SYSTEM_PROCESSOR}bat.so  lib${CMAKE_SYSTEM_PROCESSOR}Bat.so
-    PATHS ${TOOLKIT_DIR}/bin
+  find_library(CSPY_DRV_BAT
+    NAMES ${CMAKE_SHARED_LIBRARY_PREFIX}${TOOLKIT}Bat${CMAKE_SHARED_LIBRARY_SUFFIX}
+          ${CMAKE_SHARED_LIBRARY_PREFIX}${TOOLKIT}LibsupportUniversal${CMAKE_SHARED_LIBRARY_SUFFIX}
+    PATHS ${BIN_DIR}
     REQUIRED)
 
   # Add a test for CTest
@@ -36,11 +43,11 @@ function(iar_cspy_test TARGET RESULT)
              --debug_file=$<TARGET_FILE:${TARGET}>
              # C-SPY backend setup
              --backend
-             --semihosting
-             $<IF:$<STREQUAL:$<TARGET_PROPERTY:${TARGET},CPU_$<CONFIG>>,Cortex-M55.no_se.no_mve>,--cpu=Cortex-M55.no_se,--cpu=$<TARGET_PROPERTY:${TARGET},CPU_$<CONFIG>>>
-             --fpu=$<TARGET_PROPERTY:${TARGET},FPU_$<CONFIG>>
-             --endian=$<TARGET_PROPERTY:${TARGET},END_$<CONFIG>>
-             $<IF:$<STREQUAL:$<TARGET_PROPERTY:${TARGET},END_$<CONFIG>>,big>,--BE8,>)
+               --semihosting
+               $<IF:$<STREQUAL:$<TARGET_PROPERTY:${TARGET},CPU_$<CONFIG>>,Cortex-M55.no_se.no_mve>,--cpu=Cortex-M55.no_se,--cpu=$<TARGET_PROPERTY:${TARGET},CPU_$<CONFIG>>>
+               --fpu=$<TARGET_PROPERTY:${TARGET},FPU_$<CONFIG>>
+               --endian=$<TARGET_PROPERTY:${TARGET},END_$<CONFIG>>
+               $<IF:$<STREQUAL:$<TARGET_PROPERTY:${TARGET},END_$<CONFIG>>,big>,--BE8,>)
 
   # Set the test to interpret a C-SPY's message containing `SUCCESS`
   set_tests_properties(${TARGET} PROPERTIES PASS_REGULAR_EXPRESSION "${RESULT}")
