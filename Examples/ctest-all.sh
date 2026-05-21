25d2da420fed76eb1eb86660b0a5aa9a1aa56059
Felipe Torrezan <felipe.torrezan@qt.io>
2026-05-21 13:17:43 +0200

    examples: Update CMake scripts

--- Examples/ctest-all.sh
+++ Examples/ctest-all.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
 
-CFG+=(cortexM0l cortexM0b cortexM3l cortexM3b cortexM4l cortexM4b cortexM4lf cortexM4bf cortexM7l cortexM7b cortexM7ls cortexM7bs cortexM7lf cortexM7bf ARMv8MBLl ARMv8MMLl ARMv8MMLldfsp ARMv8MMLldfdp ARMv81MMLld ARMv81MMLldfdp )
+set -e
 
-for a in ${CFG[@]}; do
-  ctest --test-dir build --build-config $a $1
+CFG+=(
+  cortexM0l
+  cortexM0b
+  cortexM3l
+  cortexM3b
+  cortexM4l
+  cortexM4b
+  cortexM4lf
+  cortexM4bf
+  cortexM7l
+  cortexM7b
+  cortexM7ls
+  cortexM7bs
+  cortexM7lf
+  cortexM7bf
+  ARMv8MBLl
+  ARMv8MMLl
+  ARMv8MMLldfsp
+  ARMv8MMLldfdp
+  ARMv81MMLld
+  ARMv81MMLldfdp
+)
+
+# Run examples for all build configuration
+for cfg in ${CFG[@]}; do
+  echo "=============================================================================="
+  echo "--- Testing build configuration: ${cfg}..."
+  ctest --test-dir build --build-config ${cfg} $1
 done
 
